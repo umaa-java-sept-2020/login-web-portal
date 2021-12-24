d2e9623eb0b3710073ffa90cc4252c5aed8873a4
r0p06np <rabindra.kumar.patra@walmart.com>
2021-12-24 20:36:42 +0530

    added login root component

---
+++ src/app/components/login-root/login-root.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-root',
+  templateUrl: './login-root.component.html',
+  styleUrls: ['./login-root.component.css']
+})
+export class LoginRootComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
