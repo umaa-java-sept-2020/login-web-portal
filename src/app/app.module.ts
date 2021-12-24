6ecb346e1010c91939f76fddac701fb969296350
r0p06np <rabindra.kumar.patra@walmart.com>
2021-12-24 20:36:42 +0530

    added login root component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginRootComponent } from './components/login-root/login-root.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginRootComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LoginRootComponent]
 })
 export class AppModule { }
